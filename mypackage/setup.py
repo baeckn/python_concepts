d95d2e3d2c4408516d934828f625c79adf502c54
baeckn <jhbaeck94@gmail.com>
2021-12-01 22:39:14 +0900

    python concepts

---
+++ mypackage/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "myfirstpackage",
+    packages = find_packages(),
+    include_package_data = True,
+    version = "0.0.1",
+    author = "JonghoBaeck",
+    author_email = "jhbaeck94@gmail.com",
+    zip_safe = False,
+)
